9c121c7878bdfa86ea54277d3b074a19b020363c
thuong_bv <thuong_bv@care-connect.vn>
2024-04-03 10:33:57 +0700

    init project

---
+++ SwiftSelfLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSelfLearning
+//
+//  Created by Bui Van Thuong on 03/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSelfLearning/SwiftSelfLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSelfLearningApp.swift
+//  SwiftSelfLearning
+//
+//  Created by Bui Van Thuong on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSelfLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
